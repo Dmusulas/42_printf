26bb48528d57771c88109c5c1b7f6b4eba2b9b6d
Dovydas Musulas <dmusulas@student.42berlin.de>
2023-11-30 17:30:33 +0100

    (refact): reducing bloat

--- srcs/ft_convert_u.c
+++ srcs/ft_convert_u.c
@@ -6,52 +6,11 @@
 /*   By: dmusulas <dmusulas@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 13:08:01 by dmusulas          #+#    #+#             */
-/*   Updated: 2023/11/30 16:20:34 by dmusulas         ###   ########.fr       */
+/*   Updated: 2023/11/30 17:30:12 by dmusulas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/ft_printf.h"
-
-static int	count_len(unsigned int n);
-
-static char	*generate_unsigned(unsigned int n)
-{
-	char	*str;
-	int		len;
-
-	len = count_len(n);
-	str = malloc((len + 1) * sizeof(char));
-	if (!str)
-		return (NULL);
-	str[len] = '\0';
-	if (n == 0)
-		str[0] = '0';
-	while (len && n != 0)
-	{
-		str[len - 1] = n % 10 + '0';
-		n /= 10;
-		len--;
-	}
-	return (str);
-}
-
-static int	count_len(unsigned int nbr)
-{
-	int	len;
-
-	len = 0;
-	if (nbr <= 0)
-	{
-		nbr = -nbr;
-		len++;
-	}
-	while (nbr != 0)
-	{
-		nbr /= 10;
-		len++;
-	}
-	return (len);
-}
 
 int	ft_convert_u(unsigned int d)
 {
@@ -59,7 +18,7 @@ int	ft_convert_u(unsigned int d)
 	char	*ret;
 
 	len = 0;
-	ret = generate_unsigned(d);
+	ret = generate_num_base(d, 10);
 	len = ft_strlen(ret);
 	ft_putstr_fd(ret, 1);
 	free(ret);
